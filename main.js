8c5b1006756aa30df0997d7fd026425a07651624
Jesse Ling <jesse.ryan.ling@gmail.com>
2019-02-23 11:51:22 -0700

    Completes list lesson

--- main.js
+++ main.js
@@ -2,6 +2,22 @@ var app = new Vue({
   el: '#app',
   data: {
     product: 'Socks',
-    description: 'A pair of warm, fuzzy socks.'
+    description: 'A pair of warm, fuzzy socks.',
+    image: './assets/vmSocks-green-onWhite.jpg',
+    link: 'http://google.com',
+    inventory: 11,
+    onSale: false,
+    details: ["80% cotton", "20% polyester", "Gender-neutral"],
+    variants: [
+      {
+        variantId: 2234,
+        variantColor: "green"
+      },
+      {
+        variantId: 2235,
+        variantColor: "blue"
+      }
+    ],
+    sizes: ["S", "M", "L", "XL"]
   }
 })
